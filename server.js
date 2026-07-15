556bca6d4365bb03f94ad67f96bab642ea9fbec4
ParasxTHAKUR <thakurparas020@gmail.com>
2026-07-15 19:36:39 +0530

    added few lines in the server.js so check if running on port 5000

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ const connectDB = require('./config/db');
 
 dotenv.config();
 connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('E-Commerce API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
